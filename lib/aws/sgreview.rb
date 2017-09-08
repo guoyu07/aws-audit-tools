afc34ef431d4d488eba109b7e9ad04e7d73fe9c1
Bryan Brannigan <bjb@eightzerobits.com>
2017-09-08 13:16:27 -0400

    Added redshift enumeration to sgreview

--- lib/aws/sgreview.rb
+++ lib/aws/sgreview.rb
@@ -7,7 +7,10 @@ class SGReview
         rds = Aws::RDS::Resource.new()
         efs = Aws::EFS::Client.new()
         ec = Aws::ElastiCache::Client.new()
-        
+        elb = Aws::ElasticLoadBalancing::Client.new()
+        elb2 = Aws::ElasticLoadBalancingV2::Client.new()
+        redshift = Aws::Redshift::Client.new()
+
         rds_vpc_group = {}
         rds.db_instances().each do |instance|
             instance.vpc_security_groups.each do |group|
@@ -29,12 +32,33 @@ class SGReview
                 end
             end
         end
+        
         ec_vpc_group = {}
         ec.describe_cache_clusters().each do |clus|
             clus.cache_clusters.each do |clu|
                 clu.security_groups.each do |ec_sg|
                     (ec_vpc_group[ec_sg.security_group_id] ||= []) << clu.cache_cluster_id
                 end
+            end
+        end
+
+        elb_vpc_group = {}
+        elb.describe_load_balancers().load_balancer_descriptions.each do |elbs|
+            elbs.security_groups.each do |elb_sg|
+                (elb_vpc_group[elb_sg] ||= []) << elbs.load_balancer_name
+            end
+        end
+        elb2.describe_load_balancers().load_balancers.each do |elb2s|
+            elb2s.security_groups.each do |elb2_sg|
+                (elb_vpc_group[elb2_sg] ||= []) << elb2s.load_balancer_name
+            end
+
+        end
+
+        redshift_vpc_group = {}
+        redshift.describe_clusters().clusters.each do |rs|
+            rs.vpc_security_groups.each do |rs_sg|
+                (redshift_vpc_group[rs_sg.vpc_security_group_id] ||= []) << rs.cluster_identifier
             end
         end
 
@@ -63,12 +87,30 @@ class SGReview
                 puts "\nNo instances in group"
             end
 
+            puts "\nELB Instanced assigned in Security Group"
+
+            if elb_vpc_group[sg.id]
+                puts elb_vpc_group[sg.id]
+            else
+                puts "No ELB instances in group"
+            end
+            
             puts "\nRDS Instances assigned in Security Group"
             
             if rds_vpc_group[sg.id]
                 puts rds_vpc_group[sg.id]
             else
                 puts "No RDS instances in group"
+            end
+
+            print "\n"
+            
+            puts "\Redshift Instances assigned in Security Group"
+            
+            if redshift_vpc_group[sg.id]
+                puts redshift_vpc_group[sg.id]
+            else
+                puts "No Redshift instances in group"
             end
 
             print "\n"
