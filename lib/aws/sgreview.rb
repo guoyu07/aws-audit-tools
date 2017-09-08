f5bcdc60813580e36114c56a05862a2d0f76f8a3
Bryan Brannigan <bjb@eightzerobits.com>
2017-09-08 11:27:33 -0400

    Added EFS security group enumeration

--- lib/aws/sgreview.rb
+++ lib/aws/sgreview.rb
@@ -5,15 +5,30 @@ class SGReview
     def getAllSG()
         ec2 = Aws::EC2::Resource.new()
         rds = Aws::RDS::Resource.new()
+        efs = Aws::EFS::Client.new()
+        
         rds_vpc_group = {}
-        rds = Aws::RDS::Resource.new()
         rds.db_instances().each do |instance|
-            #puts instance.db_instance_identifier
             instance.vpc_security_groups.each do |group|
-                #puts group.vpc_security_group_id
                 (rds_vpc_group[group.vpc_security_group_id] ||= []) << instance.db_instance_identifier
             end
         end
+
+        efs_vpc_group = {}
+        efs.describe_file_systems().each do |fs|
+            fs.file_systems.each do |fs1|
+                efs.describe_mount_targets({file_system_id: fs1.file_system_id,}).each do |mt|
+                    mt.mount_targets.each do |mt1|
+                        efs.describe_mount_target_security_groups({mount_target_id: mt1.mount_target_id}).each do |mt_sg|
+                            mt_sg.security_groups.each do |mt_sg_1|
+                                (efs_vpc_group[mt_sg_1] ||= []) << fs1.file_system_id
+                            end
+                        end
+                    end
+                end
+            end
+        end
+
         ec2.security_groups().each do |sg|
             print "\n"
             puts sg.id + " - " + sg.description + " - " + sg.group_name
@@ -49,6 +64,14 @@ class SGReview
 
             print "\n"
 
+            puts "\nElastic Files Systems in Security Group"
+
+            if efs_vpc_group[sg.id]
+                puts efs_vpc_group[sg.id]
+            else
+                puts "No EFS in group"
+            end
+
             puts "\nInbound Rules\n"
             if sg.ip_permissions.count > 0
                 sg.ip_permissions.each do |rule|
@@ -80,7 +103,37 @@ class SGReview
             else
                 puts "\nNo rules"
             end
-            #ip_permissions_egress
+            puts "Outbound Rules\n"
+            if sg.ip_permissions_egress.count > 0
+                sg.ip_permissions_egress.each do |rule|
+                    print "Source: "
+                    rule.ip_ranges.each do |range|
+                        print range.cidr_ip + ", "
+                    end
+                    rule.user_id_group_pairs.each do |group|
+                        print group.group_id + ", "
+                    end
+                    if rule.ip_protocol == "-1"
+                        rule.ip_protocol = "ALL"
+                    end
+                    if rule.from_port == nil && rule.to_port == nil
+                        port_range = "ALL"
+                    else
+                        port_range = rule.from_port.to_s + "-" + rule.to_port.to_s
+                    end
+
+                    if rule.ip_protocol == "icmp"
+                        port_range = "n/a"
+                    end
+
+
+                    print "Destination Port Range: " + port_range + " IP Protocol: " + rule.ip_protocol
+
+                    print "\n"
+                end
+            else
+                puts "\nNo rules"
+            end
         end
 
     end
